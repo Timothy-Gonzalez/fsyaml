95bf36fd92a004abf0d360ad21527657e4986fed
Timothy-Gonzalez <105177619+Timothy-Gonzalez@users.noreply.github.com>
2022-06-09 10:53:33 -0500

    Add explanatory comments

--- src/lib.rs
+++ src/lib.rs
@@ -14,10 +14,12 @@ impl Command {
 	pub fn run(&self) -> Result<String, Error> {
 		let path = Path::new(&self.root);
 
+		// Check path exists
 		if !path.exists() {
 			return Err(Error::RootDirectoryDoesNotExist(self.root.clone()));
 		}
 
+		// Check is directory
 		let metadata = fs::metadata(path)?;
 
 		if !metadata.is_dir() {
